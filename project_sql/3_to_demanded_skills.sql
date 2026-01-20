a5f6279d5b7b35ed9282dbbe5e637d576ce1b821
Parth Sharma <parth121299@gmail.com>
2026-01-20 14:47:26 +0530

    Update 3_to_demanded_skills.sql

--- project_sql/3_to_demanded_skills.sql
+++ project_sql/3_to_demanded_skills.sql
@@ -15,7 +15,7 @@ INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst' 
-    AND job_work_from_home = True 
+    AND job_location = 'India' 
 GROUP BY
     skills
 ORDER BY
@@ -23,30 +23,30 @@ ORDER BY
 LIMIT 5;
 
 /*
-Here's the breakdown of the most demanded skills for data analysts
+Here's the breakdown of the most demanded skills for data analysts in India.
 SQL and Excel remain fundamental, emphasizing the need for strong foundational skills in data processing and spreadsheet manipulation.
 Programming and Visualization Tools like Python, Tableau, and Power BI are essential, pointing towards the increasing importance of technical skills in data storytelling and decision support.
 
 [
   {
     "skills": "sql",
-    "demand_count": "7291"
+    "demand_count": "1016"
   },
   {
     "skills": "excel",
-    "demand_count": "4611"
+    "demand_count": "717"
   },
   {
     "skills": "python",
-    "demand_count": "4330"
+    "demand_count": "687"
   },
   {
     "skills": "tableau",
-    "demand_count": "3745"
+    "demand_count": "545"
   },
   {
     "skills": "power bi",
-    "demand_count": "2609"
+    "demand_count": "402"
   }
 ]
 */
